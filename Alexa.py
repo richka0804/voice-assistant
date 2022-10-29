7684d6213db5cd457cdfd65aee74f2f9f3489b91
richka0804 <116939388+richka0804@users.noreply.github.com>
2022-10-29 11:53:13 +0530

    Add files via upload

---
+++ Alexa.py
@@ -0,0 +1,10 @@
+import speech_recognition as sr
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Say somethig")
+    audio = r.listen(source)
+    voice_data = r.recognize_google(audio)
+    print(voice_data)
+
+
